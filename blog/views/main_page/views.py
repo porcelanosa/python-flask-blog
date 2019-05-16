e4e6c6c2bf8bfe7867ed3b6820f316800ce9ef0c
porcelanosa <public_mce@rambler.ru>
2019-05-16 23:42:25 +0300

    fix socket address |#5

--- blog/views/main_page/views.py
+++ blog/views/main_page/views.py
@@ -18,19 +18,19 @@ def index():
         config_name=os.getenv('FLASK_CONFIG')
     )
 
-@socketio.on('my event', namespace='/test')
+@socketio.on('my event', namespace='/websocket')
 def test_message(message):
     emit('my response', {'data': message['data']})
 
-@socketio.on('my broadcast event', namespace='/test')
+@socketio.on('my broadcast event', namespace='/websocket')
 def test_message(message):
     emit('my response', {'data': message['data']}, broadcast=True)
 
-@socketio.on('connect', namespace='/test')
+@socketio.on('connect', namespace='/websocket')
 def test_connect():
     emit('my response', {'data': 'Connected'})
 
-@socketio.on('disconnect', namespace='/test')
+@socketio.on('disconnect', namespace='/websocket')
 def test_disconnect():
     print('Client disconnected')
 
